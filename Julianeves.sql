0618d6339c7ccd01482c78f83c004bd66efbe085
Julianeves01 <julianvs2007@gmail.com>
2024-11-05 09:17:34 -0300

    feat: creation of tables

---
+++ Julianeves.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE reserva_hotel;
+\c reserva_hotel;
+
+CREATE TABLE Hospedes (
+    id_hospede SERIAL PRIMARY KEY,
+    nome VARCHAR(100),
+    documento VARCHAR(50),
+    telefone VARCHAR(20)
+);
+
+CREATE TABLE Quartos (
+    id_quarto SERIAL PRIMARY KEY,
+    numero_quarto INT UNIQUE NOT NULL,
+    tipo VARCHAR(50),
+    preco_diaria DECIMAL(10,2)
+);
+
+CREATE TABLE Reservas (
+    id_reserva SERIAL PRIMARY KEY,
+    id_hospede INT NOT NULL,
+    id_quarto INT NOT NULL,
+    data_inicio DATE NOT NULL DEFAULT CURRENT_DATE,
+    data_fim DATE DEFAULT CURRENT_DATE,
+    FOREING KEY (id_hospede) REFERENCES Hospedes (id_hospede),
+    FOREING KEY (id_quarto) REFERENCES Quartos (id_quarto),
+);
